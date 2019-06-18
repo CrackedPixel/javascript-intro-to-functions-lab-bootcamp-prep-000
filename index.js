493e27db82787aba0ce7306d33bbb7b6ff1e0a0e
Anthony Carbajal <acarbajal721@live.com>
2019-06-18 23:57:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function shout(msg){
   return msg.toUpperCase();
 }
+
+function whisper(msg){
+  return msg.toLowerCase();
+}
+
+function logShout(msg){
+  console.log(shout(msg));
+}
+
+function sayHiToGrandma(msg){
+  if (msg === msg.toLowerCase()){
+    return "I can't hear you!";
+  }else{
+    return "YES INDEED";
+  }
+}
